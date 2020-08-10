4911c31c13f9f44c32d392c630218995d9bd6524
subham <sk7prince@gmail.com>
2020-08-10 16:22:03 +0530

    package is added

---
+++ project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from "./Component/Example/Example"
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Example />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ project/src/Component/Example/Example.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Example = () => {
+    return (
+        <div>
+            <header>Logo And Tabs</header>
+            <div>
+                React Welcome to app
+    </div>
+            <footer>
+                copyright@2020
+    </footer>
+        </div>
+    )
+}
+
+
+export default Example;
